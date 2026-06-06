ea00168dbfe23cfb5be4928fbf2ac65b299a2c50
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-06-06 07:38:04 +0700

    update status options type

--- src/constants/hewan.ts
+++ src/constants/hewan.ts
@@ -2,10 +2,10 @@ import type { HewanStatus } from '../domain/entities/Hewan';
 
 export const DEFAULT_HEWAN_STATUS: HewanStatus = 'tersedia';
 
-export const HEWAN_STATUS_OPTIONS: Array<{
+export const HEWAN_STATUS_OPTIONS: {
   label: string;
   value: HewanStatus;
-}> = [
+}[] = [
   { label: 'Tersedia', value: 'tersedia' },
   { label: 'Terjual', value: 'terjual' },
 ];
